89bb087f332f4c02b7b289802b390561139a7a25
Aditya Mandyam Devashikamani <adityamdk@gmail.com>
2016-03-26 23:32:44 -0700

    Program to remove duplicates in an array
    Program to remove duplicates in an array

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Program to remove duplicates in an array
 package removeduplicates;
 
 import java.util.Arrays;
 
-/**
- *
- * @author ADITYAMD
- */
 
 public class RemoveDuplicates {
 
-    /**
-     * @param args the command line arguments
-     */
+    
    public static void main(String[] args) {
 	int[] arr = { 1, 2, 2, 3, 3,4,5,6,4,5,6,7,8,8,8,77,7,8,7 };
 	arr = removDuplicates(arr);
@@ -43,9 +33,9 @@ public static int[] removDuplicates(int[] input) {
                     i++;
 		}
 	}
-        //System.out.println("count"+(j+1));
-	int[] clean = Arrays.copyOf(input, j + 1);
        
+	int[] clean = Arrays.copyOf(input, j + 1);
+       //Reversing the contents of the array to ssort the array in descending order
         for (int k = 0; k < clean.length / 2; k++) 
         {
             int temp = clean[k];
