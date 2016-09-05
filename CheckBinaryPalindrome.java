b2844feb178340a4af1f900bbc362d6c375bce08
Aditya Mandyam Devashikamani <adityamdk@gmail.com>
2016-09-05 15:38:47 -0700

    Program to check if binary representation of a number is palindrome
    Program to check if binary representation of a number is palindrome

--- CheckBinaryPalindrome.java
+++ CheckBinaryPalindrome.java
@@ -1,43 +1,33 @@
 //Program to check if a binary representation of a given number is palindrome or not.
-package checkbinarypalindrome;
-
+package integercheckforbinaryrepresentationbeingstring;
 import java.util.Scanner;
+public class IntegerCheckforBinaryRepresentationBeingString {
 
-public class CheckBinaryPalindrome {
-
+    static boolean isPalindrome(String s)
+    {
+        int n = s.length();
+        for(int i=0;i<(n/2);i++)
+        {
+            if(s.charAt(i)!=s.charAt(n-i-1))
+            {
+                    return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int k=0;
-        System.out.println("enter the number whose binary representation is to be checked");
-        int a = sc.nextInt();
-        int saved =a;
-        StringBuilder sb =new StringBuilder();
-        while(a>0)
-        {
-            
-           k  = a%2; 
-           sb.append(k);
-           a =a/2;
-           
-        }
-        StringBuilder reversea = new StringBuilder();
-        for(int i=sb.length()-1;i>=0;--i)
-        {
-            reversea.append(sb.charAt(i));
-            
-        }
-         System.out.println("The given number is "+saved);
-            System.out.println("Binary representation is"+sb);
-            System.out.println("Binary reverse representation is"+reversea);
-        if(sb.toString().equals(reversea.toString()))
-        {
-           
-            System.out.println("the binary representation is palindrome");
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the string whose binary representation is to be checked for palindrome");
+        int number = input.nextInt();
+        //converting int to binary representation
+        String binaryRep = Integer.toBinaryString(number);
+        System.out.println("Binary representation of number is "+binaryRep);
+        boolean status = isPalindrome(binaryRep);
+        
+        if(status == true)
+            System.out.println("Number in binary representation is palindrome");
         else
-        {
-            System.out.println("the binary representation is not palindrome");
-        }
+            System.out.print("Number in binary representation is not a palindrome");
     }
     
 }
